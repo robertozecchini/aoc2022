9532bf9d378e23fe9d06e26c2f720f27a96b3d9a
Roberto Zecchini <robertozecchini@gmail.com>
2022-12-04 18:06:14 +0100

    create template folder

---
+++ template/sol.py
@@ -0,0 +1,22 @@
+def read_input(filename):
+    with open(filename) as f:
+        input = f.readlines()
+    return input
+
+def get_folder_name():
+    folders = __file__.split('/')
+    return folders[-2]
+
+def sol1(input):
+    pass
+
+def sol2(input):
+    pass
+
+if __name__ == "__main__":
+    input = read_input(f"{get_folder_name()}/input")
+    print(f"Advent Of Code 2022 - {get_folder_name()}")
+    result1 = sol1(input)
+    print("First solution is: ", result1)
+    result2 = sol2(input)
+    print("Second solution is: ", result2)
