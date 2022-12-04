d4208f9f5e4859ab6229d9bcbf64b62a010d57eb
Roberto Zecchini <robertozecchini@gmail.com>
2022-12-04 08:01:58 +0100

    day 1 solution 2

--- day01/sol.py
+++ day01/sol.py
@@ -1,4 +1,4 @@
-def sol1(input):
+def read_elves_calories(input):
     elves = []
     elves.append(0)
     for line in input:
@@ -6,11 +6,22 @@ def sol1(input):
             elves.append(0)
         else:
             elves[-1] += int(line)
+    return elves
+
+def sol1(input):
+    elves = read_elves_calories(input)
     return max(elves)
+
+def sol2(input):
+    elves = read_elves_calories(input)
+    elves.sort()
+    return sum(elves[-3:])
 
 if __name__ == "__main__":
     filename = "day01/input"
     with open(filename) as f:
         input = f.readlines()
-    result = sol1(input)
-    print(result)
+    result1 = sol1(input)
+    print("First solution is: ", result1)
+    result2 = sol2(input)
+    print("Second solution is: ", result2)
--- day01/test.py
+++ day01/test.py
@@ -1,19 +1,20 @@
 import unittest
 
 from sol import sol1
-import os
+from sol import sol2
 
 
 class TestSol(unittest.TestCase):
-    def test_sol1(self):
-        """
-        Test sol1
-        """
+    def setUp(self):
         filename = "day01/test_input"
         with open(filename) as f:
-            input = f.readlines()
-        result = sol1(input)
+            self.input = f.readlines()
+    def test_sol1(self):
+        result = sol1(self.input)
         self.assertEqual(result, 24000)
+    def test_sol2(self):
+        result = sol2(self.input)
+        self.assertEqual(result, 45000)
 
 if __name__ == '__main__':
     unittest.main()
