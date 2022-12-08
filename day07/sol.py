5b2e4c6ea04071eb73c6df06bfd7856680c0582b
Roberto Zecchini <robertozecchini@gmail.com>
2022-12-08 16:01:06 +0100

    day 7 solution 2 (not working with test case)

--- day07/sol.py
+++ day07/sol.py
@@ -33,8 +33,9 @@ def get_directories(input):
             dirs[add_dir_to_path(path, args[1])] = 0
         elif args[0].isnumeric():
             dirs_list = path.split("/")
+            # print(dirs_list)
             d_path = ""
-            for d in dirs_list[:]:
+            for d in dirs_list:
                 d_path = add_dir_to_path(d_path, d)
                 dirs[d_path] += int(args[0])
     return dirs
@@ -49,7 +50,25 @@ def sol1(input):
     return total
 
 def sol2(input):
-    pass
+    total_disk_space = 70000000
+    free_space_needed = 30000000
+    dirs = get_directories(input)
+    used_space = dirs["/"]
+    free_space = total_disk_space - used_space
+    space_to_free = free_space_needed - free_space
+    dir_to_delete = "/"
+    dir_to_delete_size = used_space
+    for dir, size in dirs.items():
+        if size >= space_to_free and size < dir_to_delete_size:
+            dir_to_delete = dir
+            dir_to_delete_size = size
+    # print(dirs)
+    # print(used_space)
+    # print(free_space)
+    # print(space_to_free)
+    # print(dir_to_delete)
+    return dir_to_delete_size
+
 
 if __name__ == "__main__":
     input = read_input(f"{get_folder_name()}/input")
