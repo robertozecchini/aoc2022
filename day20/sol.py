31cdb97f7b12d4429b897dc5280b72122b38000e
Roberto Zecchini <robertozecchini@gmail.com>
2022-12-23 06:29:36 +0100

    day 20 solution 2

--- day20/sol.py
+++ day20/sol.py
@@ -7,33 +7,45 @@ def get_folder_name():
     folders = __file__.split('/')
     return folders[-2]
 
-def sol1(input):
-    enc_file = [int(x) for x in input]
+def mix(enc_file, n):
     working_file = enc_file[:]
     file_len = len(enc_file)
     idx_list = list(range(file_len))
     unique_count = len(set(enc_file))
-    # print(unique_count)
-    for idx in range(file_len):
-        # index = working_file.index(num)
-        index = idx_list.index(idx)
-        num = working_file[index]
-        new_index = (index + num) % (file_len - 1)
-        working_file.pop(index)
-        working_file.insert(new_index, num)
-        idx_list.pop(index)
-        idx_list.insert(new_index, idx)
-        # print(working_file)
-    idx0 = working_file.index(0)
+    for i in range(n):
+        for idx in range(file_len):
+            # index = working_file.index(num)
+            index = idx_list.index(idx)
+            num = working_file[index]
+            new_index = (index + num) % (file_len - 1)
+            working_file.pop(index)
+            working_file.insert(new_index, num)
+            idx_list.pop(index)
+            idx_list.insert(new_index, idx)
+    return working_file
+
+def get_grove(l):
+    idx0 = l.index(0)
+    file_len = len(l)
     i1000 = (idx0 + 1000) % file_len
     i2000 = (idx0 + 2000) % file_len
     i3000 = (idx0 + 3000) % file_len
-    print(i1000, i2000, i3000)
+    # print(i1000, i2000, i3000)
     # print(working_file)
-    return working_file[i1000] + working_file[i2000] + working_file[i3000]
+    return l[i1000], l[i2000], l[i3000]
+
+def sol1(input):
+    enc_file = [int(x) for x in input]
+    mixed_file = mix(enc_file, 1)
+    grove_coordinates = get_grove(mixed_file)
+    return sum(grove_coordinates)
 
 def sol2(input):
-    pass
+    enc_key = 811589153
+    enc_file = [int(x)*enc_key for x in input]
+    mixed_file = mix(enc_file, 10)
+    grove_coordinates = get_grove(mixed_file)
+    return sum(grove_coordinates)
 
 if __name__ == "__main__":
     input = read_input(f"{get_folder_name()}/input")
